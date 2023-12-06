702296a20fbea355a0d9ee9dfd05ba2c9a8d230d
sujung922 <sujeon922@bu.ac.kr>
2023-12-06 20:45:11 +0900

    최종

--- src/Movie.js
+++ src/Movie.js
@@ -2,25 +2,28 @@ import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import Info from "./Info";
 import Main from "./pages/Main/Main";
-import Tes from "./pages/Main/test";
 import MovieDetails from "./MovieDetail";
 
-function Movie({moviecd, title, audiCnt, rank}) {
-    const [test, settest] = useState(true);
-    //moviecd 지워도 될듯
-    return <div>
-        <h2>영화제목 : {title} {moviecd} </h2> 
-        <h4> 관객수 : {audiCnt}, 일간 박스오피스 랭킹 : {rank}<br></br><br></br></h4>
-        <button onClick={() => settest(false)}> 상세정보 </button>
+function Movie({ moviecd, title, audiCnt, rank }) {
+  const [showDetails, setShowDetails] = useState(false);
 
+  const handleShowDetails = () => {
+    setShowDetails(true);
+  };
 
-        { test ? <div> test값 true </div> : <Main moviecd2={moviecd}/> }
-    </div>;
-};
+  return (
+    <div>
+      <h2>영화제목: {title} {moviecd}</h2>
+      <h4>관객수: {audiCnt}, 일간 박스오피스 랭킹: {rank}</h4>
+      <button onClick={handleShowDetails}>상세정보</button>
 
-
-Movie.propTypes = {
-    title: PropTypes.string.isRequired
-};
+      {showDetails ? (
+        <MovieDetails movieId={moviecd} />
+      ) : (
+        <Main moviecd2={moviecd} />
+      )}
+    </div>
+  );
+}
 
 export default Movie;
--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,20 +1,95 @@
-// import React from 'react';
+import React, { useState, useEffect } from "react";
+import { movieDetailGetFetch } from "./apis/movieDetailGetFetch";
 
-// const MovieDetails = ({ movieInfo }) => {
-//   const { title, directors, openDt, showTm, genres } = movieInfo;
+const Detail = ({ movieId }) => {
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
 
-//   return (
-//     <div>
-//       <h2>{title}</h2>
-//       <p>감독: {directors}</p>
-//       <p>개봉일: {openDt}</p>
-//       <p>상영시간: {showTm}</p>
-//       <p>장르: {genres}</p>
-//       {/* 추가적인 영화 상세 정보 표시 */}
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      try {
+        const {
+          data: {
+            movieInfoResult: { movieInfo },
+          },
+        } = await movieDetailGetFetch(movieId);
 
-// export default MovieDetails;
+        setInfo({ ...movieInfo });
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-// 이걸 어떻게 연결해서 상세페이지가 나오게 할 수 있지 ;;??
+    fetchMovieDetail();
+  }, [movieId]);
+
+  const {
+    actors,
+    audits,
+    directors,
+    genres,
+    movieNm,
+    movieNmEn,
+    prdtStatNm,
+    openDt,
+    typeNm,
+  } = info;
+
+  return loading ? (
+    <div>로딩중입니다..</div>
+  ) : (
+    <div>
+      <div>
+        {/* <h1>
+          {movieNm} ({movieNmEn})
+        </h1> */}
+        <div>
+          <div>감독</div>
+          <div>
+            {directors?.map((director, i) => (
+              <span key={i}>{director.peopleNm}<br /></span>
+            ))}
+          </div>
+        </div>
+        <div>
+          {audits?.map((rule) => rule.watchGradeNm)}
+        </div>
+          <div>
+            {openDt && (
+              <>
+                {openDt.substring(0, 4)}년 {openDt.substring(4, 6)}월 {openDt.substring(6, 8)}일 {prdtStatNm}
+              </>
+            )}
+          </div>
+
+      </div>
+
+      <div>
+        <div>장르</div>
+        <div>
+          {genres?.map((genre, i) => (
+            <div key={i}>{genre.genreNm}</div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        배우들
+        <ul style={{ listStyle: "none" }}>
+          {actors?.map((actor, i) => (
+            <li key={i}>
+              <div>
+                이름: {actor.peopleNm} 배역: {actor.cast}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>영화 유형: {typeNm}</div>
+    </div>
+  );
+};
+
+export default Detail;
